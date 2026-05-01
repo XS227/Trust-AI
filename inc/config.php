82cf9c24844e0874db23370aee2e21449eb9527b
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2026-05-01 04:53:00 +0300

    Change DB_HOST to localhost

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database config (ProISP)
-define('DB_HOST', 'mysql.cifms3sug.service.one');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'cifms3sug_db1430405');
 define('DB_USER', 'cifms3sug_db1430405');
 define('DB_PASS', 'Trust@A1');
